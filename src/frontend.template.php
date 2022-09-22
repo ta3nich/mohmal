a88f5b72203b26758f8e2ff76a4237e6893ac140
cata0nana <101589752+cata0nana@users.noreply.github.com>
2022-09-22 20:32:53 +0100

    Update frontend.template.php

--- src/frontend.template.php
+++ src/frontend.template.php
@@ -178,9 +178,9 @@
                </div>
             </nav>
             <p class="lead "> Your disposable mailbox is ready. </p>
-            <iframe data-aa='1916303' src='//ad.a-ads.com/1916303?size=728x90' style='width:728px; height:90px; border:0px; padding:0; overflow:hidden; background-color: transparent;'></iframe>
+          <!--   <iframe data-aa='1916303' src='//ad.a-ads.com/1916303?size=728x90' style='width:728px; height:90px; border:0px; padding:0; overflow:hidden; background-color: transparent;'></iframe>
                
-<!--             <div class="my-col">
+            <div class="my-col">
 
             </div> -->
 
