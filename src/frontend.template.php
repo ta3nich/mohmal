12af57f33b0a71c050f76bfd3b031a8fc5847fde
ta3nich <118207850+ta3nich@users.noreply.github.com>
2022-12-04 16:21:33 -0500

    Update frontend.template.php

--- src/frontend.template.php
+++ src/frontend.template.php
@@ -281,7 +281,7 @@
                       ?>
                <div id="empty-mailbox">
                   <p>The mailbox is empty. Checking for new emails automatically. </p>
-                  <div class="spinner">
+                  <div >
                   <iframe data-aa='2129262' src='//ad.a-ads.com/2129262?size=468x60' style='width:468px; height:60px; border:0px; padding:0; overflow:hidden; background-color: transparent;'></iframe>
                      </div>
 
