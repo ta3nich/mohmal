3583ab386754d66541f8799f0b75c31604f77c1f
ta3nich <118207850+ta3nich@users.noreply.github.com>
2025-12-13 09:34:10 -0500

    Update config.php

--- src/config.php
+++ src/config.php
@@ -1,46 +1,79 @@
 <?php
 
-// set your time zone:
+// -------------------------------------------------
+// TIMEZONE
+// -------------------------------------------------
 date_default_timezone_set('Europe/Paris');
 
-// set locale (see supported locales: https://github.com/fightbulc/moment.php#switch-locale)
+// -------------------------------------------------
+// LOCALE
+// -------------------------------------------------
 $config['locale'] = 'en_US';
 
+// -------------------------------------------------
+// DEBUG MODE (ENABLE FOR TESTING)
+// -------------------------------------------------
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
 
-// enable in production:
-error_reporting(0);
+// OPTIONAL: log errors to file
+// ini_set('log_errors', 1);
+// ini_set('error_log', __DIR__ . '/php-error.log');
 
-// enable while testing:
-//ini_set('display_errors', 1);
-//ini_set('display_startup_errors', 1);
-//error_reporting(E_ALL);
-
-
-// Change IMAP settings (check SSL flags on http://php.net/manual/en/function.imap-open.php)king00binz@gmail.com
+// -------------------------------------------------
+// IMAP SETTINGS
+// -------------------------------------------------
 $config['imap']['url'] = '{outlook.office365.com:993/imap/ssl}INBOX';
-//$config['imap']['username'] = "disp0s0@outlook.com";
-//$config['imap']['username'] = "v0v0-temp@outlook.com";
-$config['imap']['username'] = "nano-s0ft@outlook.com";
+$config['imap']['username'] = 'nano-s0ft@outlook.com';
+$config['imap']['password'] = 'testpassw0rdDZA*';
 
-$config['imap']['password'] = "testpassw0rdDZA*";
-//$config['imap']['password'] = "baba123A***";
+// -------------------------------------------------
+// EMAIL DOMAINS
+// -------------------------------------------------
+$config['domains'] = array(
+    'techxbox.eu.org',
+    'itchigho.eu.org'
+);
 
-// For gmail you can use '{imap.gmail.com:993/imap/ssl}INBOX'
-// and follow the troubleshooting at:
-// https://stackoverflow.com/a/25238515/79461
-
-// email domains, usually different from imap hostname:
-$config['domains'] = array('techxbox.eu.org','itchigho.eu.org');
-
-// $config['domains'] = array('blue-vovo.nl.eu.org','green-vovo.nl.eu.org','red-vovo.nl.eu.org','api-01.nl.eu.org','techxbox.eu.org
-// ','itchigho.eu.org');
-// When to delete old messages?
+// -------------------------------------------------
+// MESSAGE CLEANUP
+// -------------------------------------------------
 $config['delete_messages_older_than'] = '30 days ago';
 
+// -------------------------------------------------
+// BLOCKED USERNAMES
+// -------------------------------------------------
+$config['blocked_usernames'] = array(
+    'root',
+    'admin',
+    'administrator',
+    'hostmaster',
+    'postmaster',
+    'webmaster'
+);
 
-// Mails to those usernames can not be accessed:
-$config['blocked_usernames'] = array('root', 'admin', 'administrator', 'hostmaster', 'postmaster', 'webmaster');
-
-// Mails are usually show as Text and only if not available as HTML. You can turn this around to prefer HTML over text.
+// -------------------------------------------------
+// MESSAGE FORMAT
+// -------------------------------------------------
 $config['prefer_plaintext'] = true;
 
+// -------------------------------------------------
+// OPTIONAL: IMAP CONNECTION DEBUG TEST
+// -------------------------------------------------
+$imap = @imap_open(
+    $config['imap']['url'],
+    $config['imap']['username'],
+    $config['imap']['password']
+);
+
+if (!$imap) {
+    echo '<pre>';
+    echo "IMAP CONNECTION FAILED\n";
+    echo imap_last_error() . "\n";
+    print_r(imap_errors());
+    echo '</pre>';
+} else {
+    echo "IMAP connection successful.";
+    imap_close($imap);
+}
