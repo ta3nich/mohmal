934aad77d5532cc03730039617b1f8a942bee82d
ta3nich <118207850+ta3nich@users.noreply.github.com>
2026-04-06 08:56:06 +0100

    Update frontend.template.php

--- src/frontend.template.php
+++ src/frontend.template.php
@@ -125,12 +125,7 @@ function printMessageBody($email, $purifier) {
 
             <p class="lead">Your disposable mailbox is ready.</p>
 
-            <!-- BEGIN AADS AD UNIT 2433029 | 728x90 -->
-            <div id="frame-2433029" style="width:728px;margin:auto;z-index:99998;height:auto">
-                <iframe data-aa='2433029' src='//ad.a-ads.com/2433029/?size=728x90'
-                    style='border:0;padding:0;width:728px;height:90px;overflow:hidden;display:block;margin:auto'></iframe>
-            </div>
-            <!-- END AADS AD UNIT 2433029 -->
+            <iframe data-aa='2132882' src='//ad.a-ads.com/2132882?size=728x90' style='width:728px; height:90px; border:0px; padding:0; overflow:hidden; background-color: transparent;'></iframe>
 
             <div class="columnsn">
                 <div class="cardn flow1">
@@ -304,30 +299,17 @@ function printMessageBody($email, $purifier) {
         });
     </script>
 
-    <!-- BEGIN AADS AD UNIT 2433028 | 970x250 sticky bottom -->
-    <div style="position:absolute;z-index:99999">
-        <input autocomplete="off" type="checkbox" id="aadsstickymnkxk9oe" hidden>
-        <div style="padding-top:0;padding-bottom:250px;">
-            <div style="width:970px;height:250px;position:fixed;text-align:center;font-size:0;bottom:0;left:0;right:0;margin:auto">
-                <label for="aadsstickymnkxk9oe" style="top:-24px;right:0;position:absolute;border-radius:4px;background:rgba(248,248,249,0.70);padding:4px;z-index:99999;cursor:pointer">
-                    <svg fill="#000000" height="16px" width="16px" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 490 490">
-                        <polygon points="456.851,0 245,212.564 33.149,0 0.708,32.337 212.669,245.004 0.708,457.678 33.149,490 245,277.443 456.851,490 489.292,457.678 277.331,245.004 489.292,32.337"/>
-                    </svg>
-                </label>
-                <div id="frame-2433028" style="width:970px;margin:auto;z-index:99998;height:auto">
-                    <div style="width:970px;margin:auto;text-align:left;position:absolute;left:0;right:0;top:-24px">
-                        <a style="display:inline-block;font-size:13px;color:#263238;padding:4px 10px;background:#F8F8F9;text-decoration:none;border-radius:4px 4px 0 0;"
-                            id="frame-link-2433028" target="_blank"
-                            href="https://aads.com/campaigns/new?source_id=2433028&source_type=ad_unit&partner=2433028">Advertise here</a>
+    <div id="frame" style="width: 100%;margin: auto;position: relative; z-index: 99998;">
+                          <div style="width: 70%;margin:auto;text-align:left;position: absolute;left: 0;right: 0; top:-24px">
+                            <a style="display:inline-block;font-size: 13px;color: #263238;padding: 4px 10px;background: #F8F8F9;text-decoration: none; border-radius: 4px 4px 0 0;"
+                              id="frame-link"
+                              target="_blank"
+                              href="https://aads.com/campaigns/new?source_id=2420628&source_type=ad_unit&partner=2420628">
+                              Advertise here
+                            </a>
+                          </div>
+                        <iframe data-aa=2420628 src=//acceptable.a-ads.com/2420628/?size=Adaptive style='border:0; padding:0; width:70%; height:auto; overflow:hidden; margin: auto'></iframe>
                     </div>
-                    <iframe data-aa="2433028" src="//ad.a-ads.com/2433028/?size=970x250"
-                        style="border:0;padding:0;width:970px;height:250px;overflow:hidden;margin:auto"></iframe>
-                </div>
-            </div>
-            <style>#aadsstickymnkxk9oe:checked + div { display: none; }</style>
-        </div>
-    </div>
-    <!-- END AADS AD UNIT 2433028 -->
 
 </body>
 </html>
